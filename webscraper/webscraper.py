4cf2a2d13fb572bd6dd526aae28810a15a2dbc26
Echmil <emilbartels3001@gmail.com>
2026-09-16 20:50:49 +0200

    updated

--- webscraper/webscraper.py
+++ webscraper/webscraper.py
@@ -2,36 +2,36 @@ import requests
 from bs4 import BeautifulSoup, SoupStrainer
 import random
 import sys
+import time
 
 iterator = 0
 def scrapeWikiArticle(url):
+    global iterator
     headerss = {
-    "User-Agent": "Mozilla/5.0"
+    "User-Agent": "MyWikipediaCrawler/1.0"
     }
     response = requests.get(url, headers=headerss)
-    #soup = BeautifulSoup(response.text, 'html.parser')
     results = []
-
+    useless_list = []
     for link in BeautifulSoup(response.text, 'html.parser', parse_only=SoupStrainer('a')):
         if link.has_attr('href'):
             results.append(link['href'])
-    useless_list = []
+    
     for link in list(results):
         if "https://en.wikipedia.org/wiki" in link:
             useless_list.append("l")
         else:
             results.remove(link)
 
-    global iterator
     iterator = iterator + 1
 
     random_link = random.choice(results)
-    print(random_link)
     substring_last = random_link[29:]
-    
 
+    print(substring_last)
     if iterator == 10:
         sys.exit("After 10 jumps it ended at: " + substring_last)
+    time.sleep(2)
     scrapeWikiArticle(random_link)
 
 scrapeWikiArticle("https://en.wikipedia.org/wiki/Counter-Strike_2")
